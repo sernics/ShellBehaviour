6edbb992896dc2f19d056c036c83527c4b885051
sernics <alu0101389936@ull.edu.es>
2023-01-03 12:03:07 +0000

    Funciona el readline, pequeño error en el print_promt

--- main.cc
+++ main.cc
@@ -7,13 +7,11 @@
 
 int main() {
   Shell shell;
-  int last_command_status = 1;
+  int last_command_status = 0;
   shell.print_prompt(last_command_status);
   std::string line;
   int fd = open("input.txt", O_RDONLY);
-  std::error_code error = shell.read_line(0, line);
+  std::error_code error = shell.read_line(fd, line);
   std::cout << line << std::endl;
-  std::cout << error.message() << std::endl;
-  std::cout << shell.get_pending_input() << std::endl;
   return 0;
 }
