4a68e84df19ff94123cbc3e18fdd1a94c6843f4b
sernics <alu0101389936@ull.edu.es>
2023-01-07 20:21:33 +0000

    Modificada a mejor la función de cp command del Intercommand

--- InternCommands.cc
+++ InternCommands.cc
@@ -1,22 +1,25 @@
-#include "InterCommnads.h"
-#include <unistd.h>
-#include <iostream>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#include <iostream>
+
+#include "InterCommnads.h"
 
 InterCommands::InterCommands(std::vector<Command> args) : args_(args) {}
 
 command_result InterCommands::execute() {
   if (args_[0][0] == "cd") {
     return this->cd_command().return_value;
-  } if (args_[0][0] == "echo") {
+  }
+  if (args_[0][0] == "echo") {
     return this->echo_command().return_value;
   }
 }
 
-command_result InterCommands::cd_command() { 
-  int exit_code = chdir(args_[0][1].c_str()); 
+command_result InterCommands::cd_command() {
+  int exit_code = chdir(args_[0][1].c_str());
   return command_result(exit_code, 0);
 }
 
@@ -25,7 +28,7 @@ command_result InterCommands::echo_command() {
   for (int i{1}; i < this->args_[0].size(); i++) {
     result += this->args_[0][i];
     result += " ";
-  } 
+  }
   std::cout << result << std::endl;
   return command_result(0, 0);
 }
@@ -40,7 +43,22 @@ command_result InterCommands::cp_command() {
   if (!S_ISREG(src_stat.st_mode)) {
     return command_result(1, 0);
   }
-  return command_result(0, 0);
+  struct stat dest_stat;
+  if (stat(dest_path.c_str(), &dest_stat) != 0) {
+    if (src_stat.st_ino == dest_stat.st_ino &&
+        src_stat.st_dev == dest_stat.st_dev) {
+      return command_result(1, 0);
+    }
+  }
+  if (S_ISDIR(dest_stat.st_mode)) {
+    dest_path += "/";
+    dest_path += src_path;
+  }
+  int src_fd = open(src_path.c_str(), O_RDONLY);
+  if (src_fd == -1) {
+    return command_result(errno, 0);
+  }
+  
 }
 
 command_result InterCommands::mv_command() {
