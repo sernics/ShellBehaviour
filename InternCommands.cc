ba598d8f6708aa21b3b2b0879cc608e077082f4a
sernics <alu0101389936@ull.edu.es>
2023-01-07 20:16:09 +0000

    Añadida funcion para el cp pero no se si funciona

--- InternCommands.cc
+++ InternCommands.cc
@@ -1,6 +1,9 @@
 #include "InterCommnads.h"
 #include <unistd.h>
 #include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 InterCommands::InterCommands(std::vector<Command> args) : args_(args) {}
 
@@ -28,7 +31,16 @@ command_result InterCommands::echo_command() {
 }
 
 command_result InterCommands::cp_command() {
-  // TODO
+  std::string src_path = std::string(this->args_[0][1]);
+  std::string dest_path = std::string(this->args_[0][2]);
+  struct stat src_stat;
+  if (stat(src_path.c_str(), &src_stat) != 0) {
+    return command_result(errno, 0);
+  }
+  if (!S_ISREG(src_stat.st_mode)) {
+    return command_result(1, 0);
+  }
+  return command_result(0, 0);
 }
 
 command_result InterCommands::mv_command() {
