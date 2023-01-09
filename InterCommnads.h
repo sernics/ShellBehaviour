5d182059e1c357e2f566a5d5400432f63ce6b2a4
sernics <alu0101389936@ull.edu.es>
2023-01-09 11:43:37 +0000

    No me funciona el copy, voy a ir a por el mv

--- InterCommnads.h
+++ InterCommnads.h
@@ -10,4 +10,5 @@ class InterCommands {
     command_result echo_command();
     command_result cp_command();
     command_result mv_command();
+    std::string basename(const std::string& path);
 };
--- InternCommands.cc
+++ InternCommands.cc
@@ -2,6 +2,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <assert.h>
+#include <array>
+#include <limits.h>
 
 #include <iostream>
 
@@ -39,35 +42,47 @@ command_result InterCommands::echo_command() {
 }
 
 command_result InterCommands::cp_command() {
-  std::cout << "Copy" << std::endl;
-  std::string src_path = std::string(this->args_[0][1]);
-  std::string dest_path = std::string(this->args_[0][2]);
-  struct stat src_stat;
-  if (stat(src_path.c_str(), &src_stat) != 0) {
-    return command_result(errno, 0);
+  std::string src_path = args_[0][1];
+  std::string dst_path = args_[0][2];
+  struct stat src;
+  struct stat dst;
+  char buffer[1024];
+  if (stat(src_path.c_str(), &src) == -1) {
+    throw std::system_error(errno, std::system_category(),
+                            "Error al obtener el estado del archivo src_path");
   }
-  if (!S_ISREG(src_stat.st_mode)) {
-    return command_result(1, 0);
+  if (!S_ISREG(src.st_mode)) {
+    throw std::system_error(errno, std::system_category(),
+                            "El archivo no es un archivo regular");
   }
-  struct stat dest_stat;
-  if (stat(dest_path.c_str(), &dest_stat) != 0) {
-    if (src_stat.st_ino == dest_stat.st_ino &&
-        src_stat.st_dev == dest_stat.st_dev) {
-      return command_result(1, 0);
+  if (stat(dst_path.c_str(), &dst) != -1) {
+    assert(dst_path != src_path);
+    if (S_ISDIR(dst.st_mode)) {
+      dst_path += "/";
+      dst_path += this->basename(src_path);
     }
   }
-  if (S_ISDIR(dest_stat.st_mode)) {
-    dest_path += "/";
-    dest_path += src_path;
+  int fd1 = open(src_path.c_str(), O_RDONLY);
+  int fd2 = open(dst_path.c_str(), O_WRONLY | O_CREAT | O_TRUNC, src.st_mode);
+  std::cout << src_path << std::endl;
+  std::cout << dst_path << std::endl;
+  int numbytes = read(fd1, buffer, sizeof(buffer));
+  while (numbytes > 0) {
+    std::cout << numbytes << std::endl;
+    write(fd2, buffer, numbytes);
+    numbytes = read(fd1, buffer, sizeof(buffer));
   }
-  int src_fd = open(src_path.c_str(), O_RDONLY);
-  if (src_fd == -1) {
-    return command_result(errno, 0);
-  }
-  return command_result(0, 0);
+  close(fd1);
+  close(fd2);
 }
 
 command_result InterCommands::mv_command() {
   std::cout << "Move: " << std::endl;
   return command_result(-1, 0);
+}
+
+std::string InterCommands::basename(const std::string &path) {
+  std::array<char, PATH_MAX> buffer;
+  path.copy(buffer.data(), buffer.size());
+  return std::string(basename(buffer.data()));
 }
