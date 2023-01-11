041fcfa3a359b45ed00c8e1bad19fe604b928e12
sernics <alu0101389936@ull.edu.es>
2023-01-11 18:34:47 +0000

    Practica terminada

--- InterCommnads.h
+++ InterCommnads.h
@@ -4,16 +4,16 @@
 #include "command.h"
 
 class InterCommands {
-  public:
-    InterCommands(std::vector<Command> args);
-    command_result execute();
-  private:
-    std::vector<Command> args_;
-    command_result cd_command();
-    command_result echo_command();
-    command_result cp_command(bool preserve_all);
-    command_result mv_command();
-    std::string basename(const std::string& path);
+ public:
+  InterCommands(std::vector<Command> args);
+  command_result execute();
+ private:
+  std::vector<Command> args_;
+  command_result cd_command();
+  command_result echo_command();
+  command_result cp_command(bool preserve_all);
+  command_result mv_command();
+  std::string basename(const std::string& path);
 };
 
-#endif // INTERCOMMANDS_H_
+#endif  // INTERCOMMANDS_H_
